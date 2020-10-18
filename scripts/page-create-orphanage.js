f2903950cc006fa01d1fcf1ccf71f8d1122307db
lyssacavalcanti <lyssa.cavalcanti@gmail.com>
2020-10-18 19:33:40 -0300

    Coding update.

--- scripts/page-create-orphanage.js
+++ scripts/page-create-orphanage.js
@@ -14,11 +14,28 @@ map.on('click', (event) => {
     const lat = event.latlng.lat;
     const lng = event.latlng.lng;
 
+    document.querySelector('[name=lat]').value = lat;
+    document.querySelector('[name=lng]').value = lng;
+
     marker && map.removeLayer(marker)
 
     marker = L.marker([lat, lng], {icon})
     .addTo(map)
 })
 
+function addPhotoField() {
+    const container = document.querySelector('#images')
+    const fieldsContainer = document.querySelectorAll('.new-upload')
+    const newFieldContainer = fieldsContainer[fieldsContainer.length - 1].cloneNode(true)
+    const input = newFieldContainer.children[0]
+    if(input.value = ""){
+        return
+    }
+    input.value = ""
 
-   
+    container.appendChild(newFieldContainer)
+}
+
+function deleteField(event) {
+    
+}
