804b9bddaba0b994e6a3c710a69212a24a098e88
lyssacavalcanti <lyssa.cavalcanti@gmail.com>
2020-10-19 13:43:35 -0300

    Coding update.

--- src/database/fakedata.js
+++ src/database/fakedata.js
@@ -1,33 +1,33 @@
 module.exports = [
     {
         id: 1,
-        lat:"-23.9624403,-46.3399801",
-        lng:"-23.9624403,-46.3399801"
+        lat:"-23.9624403",
+        lng:"-46.3399801",
         name: "Lar das Meninas",
-        descripition: "Presta assistência a crianças de 06 a 15 anos que se encontram em situação de risco e/ou vulnerabilidade social."
+        descripition: "Presta assistência a crianças de 06 a 15 anos que se encontram em situação de risco e/ou vulnerabilidade social.",
         images: [
             "https://images.unsplash.com/photo-1527490087278-9c75be0b8052?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1347&q=80",
             "https://images.unsplash.com/photo-1565843248736-8c41e6db117b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80", "https://images.unsplash.com/photo-1519457431-44ccd64a579b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=675&q=80",
             "https://images.unsplash.com/photo-1490150028299-bf57d78394e0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
     ],
-    instructions:"Vista-se confortavelmente e traga muito amor para compartilhar."
-    opening-hours:"Horário de visitas Das 18h até às 8h",
-    open-on-weekends:"1"
-
+    instructions:"Vista-se confortavelmente e traga muito amor para compartilhar.",
+    opening_hours:"Horário de visitas Das 18h até às 8h",
+    open_on_weekends:"1"
     },
+
     {
         id: 2,
-        lat:"-23.9624403,-46.3399801",
-        lng:"-23.9624403,-46.3399801"
+        lat:"-23.9644403",
+        lng:"-46.3499801",
         name: "Lar dos Meninos",
-        descripition: "Presta assistência a crianças de 06 a 15 anos que se encontram em situação de risco e/ou vulnerabilidade social."
+        descripition: "Presta assistência a crianças de 06 a 15 anos que se encontram em situação de risco e/ou vulnerabilidade social.",
         images: [
             "https://images.unsplash.com/photo-1490065498760-2828fc7f8180?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
             "https://images.unsplash.com/photo-1488521787991-ed7bbaae773c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80", "https://images.unsplash.com/photo-1505748641491-f7ee2fd6fb4d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1401&q=80",
             "https://images.unsplash.com/photo-1541887907133-59852349d740?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
     ],
-    instructions:"Vista-se confortavelmente e traga muito amor para compartilhar."
-    opening-hours:"Horário de visitas Das 18h até às 8h",
-    open-on-weekends:"0"
+    instructions:"Vista-se confortavelmente e traga muito amor para compartilhar.",
+    opening_hours:"Horário de visitas Das 18h até às 8h",
+    open_on_weekends:"0"
     },
 ]
