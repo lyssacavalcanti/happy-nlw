79120668ab130f959fb5b3fc507ac11016584510
lyssacavalcanti <lyssa.cavalcanti@gmail.com>
2020-10-19 10:37:22 -0300

    Coding update.

--- src/server.js
+++ src/server.js
@@ -1,12 +1,17 @@
 //Importar dependências
 const express = require('express');
+const path = require('path');
 
 // Iniciando express
 const server = express()
+server
+
+// Utilizando arquivos estáticos 
+.use(express.static('public'))
 
 // Criar rota
-server.get('/', () => {
-    console.log('oi')
+.get('/', (request, response) => {
+    return response.sendFile(path.join(__dirname, 'views', 'index.html'))
 })
 
 // Ligar servidor
