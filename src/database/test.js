bb29f25f1fe14f9864378b95e13b20e9d9d9990a
lyssacavalcanti <lyssa.cavalcanti@gmail.com>
2020-10-20 16:11:59 -0300

    Coding update.

--- src/database/test.js
+++ src/database/test.js
@@ -1,32 +1,35 @@
 const Database = require('./db');
+const saveOrphanage = require('./saveOrphanage');
 
 Database.then(async db => {
-    //  Inserir dados na tabela
-    await db.run(`
-        INSERT INTO orphanages (
-            lat,
-            lng,
-            name,
-            about, 
-            whatsapp,
-            images,
-            instructions,
-            opening_hours,
-            open_on_weekends
-        ) VALUES (
-            "-23.9624403",
-            "-46.3399801",
-            "Lar das Meninas",
-            "Presta assistência a crianças de 06 a 15 anos que se encontram em situação de risco e/ou vulnerabilidade social.",
-            "9465833165",
+   //  Inserir dados na tabela
+    await saveOrphanage(db,{
+        lat:"-23.9624403",
+        lng:"-46.3699801",
+        name: "Lar das Meninas",
+        about:"Presta assistência a crianças de 06 a 15 anos que se encontram em situação de risco e/ou vulnerabilidade social.",
+        whatsapp: "9659823546",
+        images: [
             "https://images.unsplash.com/photo-1527490087278-9c75be0b8052?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1347&q=80",
-            "Vista-se confortavelmente e traga muito amor para compartilhar.",
-            "Horário de visitas Das 18h até às 8h",
-            "1"
-            );
-    `)
+            "https://images.unsplash.com/photo-1565843248736-8c41e6db117b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80", 
+            "https://images.unsplash.com/photo-1519457431-44ccd64a579b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=675&q=80",
+            "https://images.unsplash.com/photo-1490150028299-bf57d78394e0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
+        ].toString(),
+        instructions:"Vista-se confortavelmente e traga muito amor para compartilhar.",
+        opening_hours:"Horário de visitas Das 18h até às 8h",
+        open_on_weekends:"1"
+    })
+
     // Consultar dados na tabela
     const selectedOrphanages = await db.all("SELECT * FROM orphanages")
     console.log(selectedOrphanages)
+
+    // Consultar somente 1 orfanato pelo id
+    //const orphanage = await db.all('SELECT * FROM orphanages WHERE id = "2"')
+    //console.log(orphanage)
+
+    // Deletar dado da tabela
+    //await db.run("DELETE FROM orphanages WHERE id = '8'")
+    //await db.run("DELETE FROM orphanages WHERE id = '9'") 
 })
 
--- src/pages.js
+++ src/pages.js
@@ -1,14 +1,37 @@
-const orphanages = require('./database/fakedata.js');
+const Database = require('./database/db');
+const saveOrphanage = require('./database/saveOrphanage')
 
 module.exports = { 
     index(req,res) {
         return res.render('index')
     },
-    orphanage(req,res) {
-        return res.render('orphanage')
+    
+    async orphanage(req,res) {
+        const id = req.query.id
+       try { 
+            const db = await Database;
+            const results = await db.all(`SELECT * FROM orphanages WHERE id = "${id}"`)
+            const orphanage = results[0]
+            orphanage.images = orphanage.images.split(",")
+            orphanage.firstImage = orphanage.images[0]
+
+            return res.render('orphanage', {orphanage})   
+       } catch (error) {
+           console.log(error);
+           return res.send('Erro no banco de dados!')
+       }
     },
-    orphanages(req,res){
+
+    async orphanages(req,res){
+        try {
+            const db = await Database; 
+            const orphanages = await db.all("SELECT * FROM orphanages")
         return res.render('orphanages', {orphanages})
+            
+        } catch (error) {
+            console.log(error)
+            return res.send('Erro no banco de dados.')
+        }        
     },
     createOrphanage(req, res) {
         return res.render('create-orphanage')
